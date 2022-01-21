a8d4bd3b5b84e3685e1af6ec6c6e096215161c43
Mimi <76797616+rumpeltin@users.noreply.github.com>
2022-01-21 02:45:22 +0000

    added objects & arrays with pokemon examples

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
-alert('Hello world');
-let favoriteFood = 'strawberries';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [
+  {name: "bulbasaur",
+  height: 0.7,
+  type: "grass"},
+
+  {name: "eevee",
+  height: 0.3,
+  type: ["water", "speed"]},
+
+  {name: "wartortle",
+  height: 1,
+  type: "water","defense"},
+
+  {name: "fearow",
+  height: 1.2,
+  type: ["flying", "speed"]},
+
+  {name: "arbok",
+  height: 3.5,
+  type: "poison"},
+
+  {name: "tentacruel",
+  height: 1.6,
+  type: ["water", "poison"]},
+
+  {name: "dratini",
+  height: 1.8,
+  type: ["dragon", "attack"]}
+];
