7b58a0e2659b86e0beb1bd00d14b531416db0f55
Mimi <76797616+rumpeltin@users.noreply.github.com>
2022-02-10 17:13:30 +0100

    trying to add js for loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,46 @@ let pokemonList = [];
 pokemonList = [
   {name: "bulbasaur",
   height: 0.7,
-  type: "grass"},
+  type: "grass"
+  },
 
   {name: "eevee",
   height: 0.3,
-  type: ["water", "speed"]},
+  type: ["water", "speed"]
+  },
 
   {name: "wartortle",
   height: 1,
-  type: "water","defense"},
+  type: "water","defense"
+  },
 
   {name: "fearow",
   height: 1.2,
-  type: ["flying", "speed"]},
+  type: ["flying", "speed"]
+  },
 
   {name: "arbok",
   height: 3.5,
-  type: "poison"},
+  type: "poison"
+  },
 
   {name: "tentacruel",
   height: 1.6,
-  type: ["water", "poison"]},
+  type: ["water", "poison"]
+  },
 
   {name: "dratini",
   height: 1.8,
-  type: ["dragon", "attack"]}
+  type: ["dragon", "attack"]
+  }
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 1) {
+    console.log(pokemonList[i].name + " is a smol boi.");
+  } else if (pokemonList[i].height > 1 && pokemonList[i].height < 3) {
+    console.log(pokemonList[i].name + " is a good-sized boi.");
+  } else {
+    console.log(pokemonList[i].name + " wat ein Oschi!");
+  }
+}
