2f60dd282aa9a0410d16f2d1d5b5a2f12ec348af
Mimi <76797616+rumpeltin@users.noreply.github.com>
2022-02-11 17:16:46 +0100

    added line break to loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,45 @@
 let pokemonList = [];
 pokemonList = [
-  {name: "bulbasaur",
+  {name: "Bulbasaur",
   height: 0.7,
   type: "grass"
   },
 
-  {name: "eevee",
+  {name: "Eevee",
   height: 0.3,
   type: ["water", "speed"]
   },
 
-  {name: "wartortle",
+  {name: "Wartortle",
   height: 1,
   type: ["water", "defense"]
   },
 
-  {name: "fearow",
+  {name: "Fearow",
   height: 1.2,
   type: ["flying", "speed"]
   },
 
-  {name: "arbok",
+  {name: "Arbok",
   height: 3.5,
   type: "poison"
   },
 
-  {name: "tentacruel",
+  {name: "Tentacruel",
   height: 1.6,
   type: ["water", "poison"]
   },
 
-  {name: "dratini",
+  {name: "Dratini",
   height: 1.8,
   type: ["dragon", "attack"]
   }
 ];
 
 for (let i=0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height < 1) {
-    console.log(pokemonList[i].name + " is a smol boi.");
-  } else if (pokemonList[i].height > 1 && pokemonList[i].height < 3) {
-    console.log(pokemonList[i].name + " is a good-sized boi.");
+  if (pokemonList[i].height > 2) {
+    document.write(" " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" + '<br/>');
   } else {
-    console.log(pokemonList[i].name + " is ein Oschi!");
+    document.write(" " + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + '<br/>');
   }
 }
