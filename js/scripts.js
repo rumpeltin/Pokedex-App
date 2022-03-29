eb181927be66c28d050492e84fd3b68b350e3e8d
Mimi <76797616+rumpeltin@users.noreply.github.com>
2022-03-29 18:39:43 +0100

    replaced for loop w/forEach & added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,64 @@
-// adding list of data entries
-let pokemonList = [];
-pokemonList = [
-  {name: "Bulbasaur",
-  height: 0.7,
-  type: "grass"
-  },
+  // adding list of data entries
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: "Bulbasaur",
+    height: 0.7,
+    type: "grass"
+    },
 
-  {name: "Eevee",
-  height: 0.3,
-  type: ["water", "speed"]
-  },
+    {name: "Eevee",
+    height: 0.3,
+    type: ["water", "speed"]
+    },
 
-  {name: "Wartortle",
-  height: 1,
-  type: ["water", "defense"]
-  },
+    {name: "Wartortle",
+    height: 1,
+    type: ["water", "defense"]
+    },
 
-  {name: "Fearow",
-  height: 1.2,
-  type: ["flying", "speed"]
-  },
+    {name: "Fearow",
+    height: 1.2,
+    type: ["flying", "speed"]
+    },
 
-  {name: "Arbok",
-  height: 3.5,
-  type: "poison"
-  },
+    {name: "Arbok",
+    height: 3.5,
+    type: "poison"
+    },
 
-  {name: "Tentacruel",
-  height: 1.6,
-  type: ["water", "poison"]
-  },
+    {name: "Tentacruel",
+    height: 1.6,
+    type: ["water", "poison"]
+    },
 
-  {name: "Dratini",
-  height: 1.8,
-  type: ["dragon", "attack"]
-  }
-];
-
-// create function to write above list in document & console
-function printArrayDetails(list){
-  for (let i=0; i < list.length; i++) {
-    if (list[i].height > 2) {
-      document.write(list[i].name + " (height: " + list[i].height + ") - Wow, that's big!" + '<br/>');
-      console.log(list[i].name + " (height: " + list[i].height + ") - Wow, that's big!");
-    } else {
-      // or document.write("<p>" + list[i].name + " (height: " + list[i].height + ")" + "</p>")
-      document.write(list[i].name + " (height: " + list[i].height + ")" + '<br/>');
-      console.log(list[i].name + " (height: " + list[i].height + ")");
+    {name: "Dratini",
+    height: 1.8,
+    type: ["dragon", "attack"]
     }
-  }
-}
+  ];
 
-printArrayDetails(pokemonList);
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+//  forEach displaying list in document & console
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height > 2) {
+    document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow, that's big!" + '<br/>');
+    console.log(pokemon.name + " (height: " + pokemon.height + ") - Wow, that's big!");
+  } else {
+    // or document.write("<p>" + list[i].name + " (height: " + list[i].height + ")" + "</p>")
+    document.write(pokemon.name + " (height: " + pokemon.height + ")" + '<br/>');
+    console.log(pokemon.name + " (height: " + pokemon.height + ")");
+  }
+});
